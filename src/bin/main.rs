0a0262edaf70db929725f28fc0c00894f963c36a
pierreyoda <pierreyoda@users.noreply.github.com>
2015-05-24 18:31:43 +0200

    CB-RES instructions implemented
    Also added the 'MemCpuTest' struct to improve the basic instruction
    testing in the main.rs crate.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,31 +1,41 @@
 extern crate rustboylib;
 
-struct MemTest { i: u32 }
-impl rustboylib::memory::Memory for MemTest {
+/// Simple CPU instruction testing facility leveraging the 'Memory' trait to
+/// sequentially feed an array of opcodes to a 'Cpu' instance.
+struct MemCpuTest {
+    ic           : usize,
+    instructions : Vec<u8>,
+}
+impl rustboylib::memory::Memory for MemCpuTest {
     fn read_byte(&mut self, address: u16) -> u8 {
-        self.i += 1;
-        match self.i {
-            1 => 0x51, // ldrr_dc
-            2 => 0x62, // ldrr_hd
-            3 => 0xCB,
-            4 => 0x31, // SWAP rC
-            5 => 0xCB,
-            6 => 0xE7, // SET 4,rA
-            7 => 0xCB,
-            8 => 0xC0, // SET 0,rB
-            _ => 0x10, // stop
+        if self.ic < self.instructions.len() {
+            let opcode = self.instructions[self.ic];
+            self.ic += 1;
+            opcode
+        } else {
+            println!("MemCpuTest : instructions end reached");
+            0x10
         }
     }
-    fn write_byte(&mut self, address: u16, byte: u8) {
-    }
+    fn write_byte(&mut self, address: u16, byte: u8) {}
     fn read_word(&mut self, address: u16) -> u16 { 0 }
     fn write_word(&mut self, address: u16, word: u16) { }
 }
 
 fn main() {
     // CPU crude test
-    let mapper = MemTest { i: 0}; //rustboylib::mmu::MMU;
-    let mut cpu = rustboylib::cpu::Cpu::new(mapper);
+    let test_opcodes = vec![
+        0x51, // ldrr_dc
+        0x62, // ldrr_hd
+        0xCB, 0x31, // SWAP rC
+        0xCB, 0xE7, // SET 4,rA
+        0xCB, 0xC0, // SET 0,rB
+        0xCB, 0xA7, // RES 4,rA
+        0xCB, 0x80, // RES 0,rB
+        0xCB, 0xA1, // RES 4,rC
+    ];
+    let test_memory = MemCpuTest {ic: 0, instructions: test_opcodes};
+    let mut cpu = rustboylib::cpu::Cpu::new(test_memory);
     cpu.regs.c = 0xAB;
     println!("cpu regs : {:?}", cpu.registers());
     cpu.step();
@@ -37,6 +47,12 @@ fn main() {
     println!("cpu regs : {:?}", cpu.registers());
 
     println!("SET");
+    cpu.step();
+    cpu.step();
+    println!("cpu regs : {:?}", cpu.registers());
+
+    println!("RES");
+    cpu.step();
     cpu.step();
     cpu.step();
     println!("cpu regs : {:?}", cpu.registers());
--- src/cpu_cb_ops.rs
+++ src/cpu_cb_ops.rs
@@ -20,13 +20,27 @@ macro_rules! swap {
     })
 }
 
+macro_rules! impl_RES_b_r_x {
+    ($s: ident, $b: expr, $x: ident) => (
+        $s.regs.$x &= !(1 << $b);
+        return 2;
+    )
+}
+macro_rules! impl_RES_b_HLm {
+    ($s: ident, $b: expr) => ({
+        let hl = ($s.regs.h as u16) << 8 + $s.regs.l as u16;
+        let v = $s.mem.read_byte(hl) & !(1 << $b);
+        $s.mem.write_byte(hl, v);
+        return 4;
+    })
+}
+
 macro_rules! impl_SET_b_r_x {
     ($s: ident, $b: expr, $x: ident) => (
         $s.regs.$x |= (1 << $b);
         return 2;
     )
 }
-
 macro_rules! impl_SET_b_HLm {
     ($s: ident, $b: expr) => ({
         let hl = ($s.regs.h as u16) << 8 + $s.regs.l as u16;
@@ -62,6 +76,84 @@ impl<M> Cpu<M> where M: Memory {
         self.mem.write_byte(address, temp_byte);
         4
     }
+
+    //
+    // --- RES ---
+    //
+    // RES b, X : set to 0 the byte of index b in register X
+    pub fn RES_0_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, b); }
+    pub fn RES_0_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, c); }
+    pub fn RES_0_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, d); }
+    pub fn RES_0_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, e); }
+    pub fn RES_0_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, h); }
+    pub fn RES_0_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, l); }
+    pub fn RES_0_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 0, a); }
+
+    pub fn RES_1_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, b); }
+    pub fn RES_1_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, c); }
+    pub fn RES_1_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, d); }
+    pub fn RES_1_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, e); }
+    pub fn RES_1_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, h); }
+    pub fn RES_1_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, l); }
+    pub fn RES_1_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 1, a); }
+
+    pub fn RES_2_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, b); }
+    pub fn RES_2_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, c); }
+    pub fn RES_2_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, d); }
+    pub fn RES_2_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, e); }
+    pub fn RES_2_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, h); }
+    pub fn RES_2_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, l); }
+    pub fn RES_2_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 2, a); }
+
+    pub fn RES_3_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, b); }
+    pub fn RES_3_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, c); }
+    pub fn RES_3_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, d); }
+    pub fn RES_3_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, e); }
+    pub fn RES_3_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, h); }
+    pub fn RES_3_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, l); }
+    pub fn RES_3_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 3, a); }
+
+    pub fn RES_4_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, b); }
+    pub fn RES_4_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, c); }
+    pub fn RES_4_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, d); }
+    pub fn RES_4_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, e); }
+    pub fn RES_4_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, h); }
+    pub fn RES_4_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, l); }
+    pub fn RES_4_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 4, a); }
+
+    pub fn RES_5_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, b); }
+    pub fn RES_5_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, c); }
+    pub fn RES_5_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, d); }
+    pub fn RES_5_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, e); }
+    pub fn RES_5_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, h); }
+    pub fn RES_5_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, l); }
+    pub fn RES_5_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 5, a); }
+
+    pub fn RES_6_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, b); }
+    pub fn RES_6_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, c); }
+    pub fn RES_6_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, d); }
+    pub fn RES_6_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, e); }
+    pub fn RES_6_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, h); }
+    pub fn RES_6_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, l); }
+    pub fn RES_6_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 6, a); }
+
+    pub fn RES_7_r_b(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, b); }
+    pub fn RES_7_r_c(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, c); }
+    pub fn RES_7_r_d(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, d); }
+    pub fn RES_7_r_e(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, e); }
+    pub fn RES_7_r_h(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, h); }
+    pub fn RES_7_r_l(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, l); }
+    pub fn RES_7_r_a(&mut self) -> CycleType { impl_RES_b_r_x!(self, 7, a); }
+
+    // RES b, (HL) : set to 0 the byte of index b in (HL)
+    pub fn RES_0_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 0); }
+    pub fn RES_1_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 1); }
+    pub fn RES_2_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 2); }
+    pub fn RES_3_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 3); }
+    pub fn RES_4_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 4); }
+    pub fn RES_5_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 5); }
+    pub fn RES_6_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 6); }
+    pub fn RES_7_HLm(&mut self) -> CycleType { impl_RES_b_HLm!(self, 7); }
 
     //
     // --- SET ---
